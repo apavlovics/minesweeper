d043066e332afc6e519967b7c497e9b03d9fc9f6
Andrejs Pavlovics <apavlovics2@evolutiongaming.com>
2020-06-27 23:59:45 +0300

    Migrate Cell to ES6 class

--- engine/cell.js
+++ engine/cell.js
@@ -1,35 +1,40 @@
-function Cell(value, visited, marked) {
-  if (value == 'M') {
-    this.value = value
-  } else {
-    value = parseInt(value)
-    if (value >= 0 && value <= 8) this.value = value
-    else throw 'Cell value is not valid'
+class Cell {
+  constructor(value, visited, marked) {
+    if (value == 'M') {
+      this.value = value
+    } else {
+      value = parseInt(value)
+      if (value >= 0 && value <= 8) this.value = value
+      else throw 'Cell value is not valid'
+    }
+
+    // Loosely convert to boolean
+    this.visited = visited == true
+    this.marked = marked == true
+    if (this.visited && this.marked) {
+      throw 'Cell flags are not valid'
+    }
   }
 
-  // Loosely convert to boolean
-  this.visited = visited == true
-  this.marked = marked == true
-  if (this.visited && this.marked) throw 'Cell flags are not valid'
-
-  this.hasMine = function() {
+  hasMine() {
     return this.value == 'M'
   }
 
-  this.toString = function() {
+  toString() {
 
     // Convert boolean to integer
     const visitedInt = +this.visited
     const markedInt = +this.marked
-    return String(value) + String(visitedInt) + String(markedInt)
+    return String(this.value) + String(visitedInt) + String(markedInt)
   }
-}
 
-Cell.fromString = function(string) {
-  if (string.length != 3) throw 'There must be 3 cell attributes'
-
-  const value = string.substr(0, 1)
-  const visited = string.substr(1, 1)
-  const marked = string.substr(2, 1)
-  return new Cell(value, visited, marked)
+  static fromString(string) {
+    if (string.length != 3) {
+      throw 'There must be 3 cell attributes'
+    }
+    const value = string.substr(0, 1)
+    const visited = string.substr(1, 1)
+    const marked = string.substr(2, 1)
+    return new Cell(value, visited, marked)
+  }
 }
