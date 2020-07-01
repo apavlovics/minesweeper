c31ee8366e5a28291ad6ddcfa9c177ae052762ce
Andrejs Pavlovics <apavlovics2@evolutiongaming.com>
2020-07-01 22:05:19 +0300

    Further optimize CSS

--- js/game.js
+++ js/game.js
@@ -216,7 +216,7 @@ export class Game {
           cell.marked = false
           Controls.mineCount++
         }
-        td.empty().append('<div class="mine mine-small-white"></div>')
+        td.empty().append('<div class="mine mine-revealed"></div>')
       }
     }
 
