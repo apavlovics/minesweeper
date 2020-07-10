cf24f942e4a9d21d08460231b1c1e40e12b8ee58
Andrejs Pavlovics <andrey.pavlovich@me.com>
2020-07-10 21:12:23 +0300

    Add main.js

--- compress.sh
+++ compress.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd js
-rollup game.js --file ../target/minesweeper.js
+rollup main.js --file ../target/minesweeper.js
 terser --compress --mangle --output ../target/minesweeper.min.js -- ../target/minesweeper.js
