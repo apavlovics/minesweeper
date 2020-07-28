21951f45ac173429231d5fcaa8d003b5c97617a0
Andrejs Pavlovics <apavlovics2@evolutiongaming.com>
2020-07-28 14:56:56 +0300

    Add output to compress.sh

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 cd js
+echo "Will bundle all JavaScript files into one and minify the code..."
 rollup main.js --file ../minesweeper.js
 terser --compress --mangle --output ../minesweeper.min.js -- ../minesweeper.js
+echo "Done: minesweeper.js and minesweeper.min.js are generated"
